487986f5656e7e403d6ee1fcfa8e0f95293f7eb3
Herman Combrink <herman@coms.net.nz>
2021-01-01 15:20:35 +1300

    ..

--- nuxt.config.js
+++ nuxt.config.js
@@ -81,6 +81,9 @@ export default {
     port: 5700, // default: 3000
     host: '0.0.0.0' // default: localhost
   },
+  pwa: {
+    icon: false // disables the icon module
+  },
   moment: {},
   vue: {
     config: {
